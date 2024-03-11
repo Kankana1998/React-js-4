c8cfde54cfb70bcf34665f73834abaa6362ce2c8
Kankana Nath <kankana109@gmail.com>
2024-03-11 22:30:55 +0530

    header 1.0

--- Swiggy API/src/App.jsx
+++ Swiggy API/src/App.jsx
@@ -3,10 +3,29 @@ import './App.css'
 
 function App() {
   
+  const Header =() => {
+    return (
+      <div className='header'>
+        <div className='logo-container'>
+        <img className='logo'
+        src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=" alt="logo" />
+        </div>
+        <div className='nav-items'>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+   
+    )
+  }
 
   return (
     <>
-     <h1>Hello World</h1>
+     <Header />
     </>
   )
 }
