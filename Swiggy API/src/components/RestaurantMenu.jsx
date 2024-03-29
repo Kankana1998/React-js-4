142e0f32281b3ed53cc3791c6c3e4f3d6f38a6fb
Kankana Nath <kankana109@gmail.com>
2024-03-29 23:34:44 +0530

    categories

--- Swiggy API/src/components/RestaurantMenu.jsx
+++ Swiggy API/src/components/RestaurantMenu.jsx
@@ -14,6 +14,13 @@ const resInfo = useRestaurantMenu(resId);
 const {name, cuisines, costForTwoMessage} = resInfo?.cards[0]?.card?.card?.info;
 
 const { itemCards } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+
+console.log(resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
+
+const categories = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c) => c.card?.card?.["@type"] === "type");
+
+console.log(categories);
+
     return (
         <div className="menu">
             <h1>{name}</h1>
